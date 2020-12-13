fd8b3f612dad6ec2af3cceafdb9dd1bf6e917fb3
aleksandar <aca.djordjevic1113@gmail.com>
2020-12-13 10:58:18 -0800

    buttons to save and buy tickets

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -3,8 +3,59 @@ import './Card.css'
 import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import {getClub} from '../store/actions/clubAction'
+import { Button } from '@material-ui/core'
+
+
+///material UI
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+
+//material UI
 
 const Card = ({ party, clubs }) => {
+
+    const classes = useStyles();
 
     console.log(clubs)
     return (
@@ -16,6 +67,13 @@ const Card = ({ party, clubs }) => {
                         <h2>{party.description}</h2>
                         {/* <h4>{home.description}</h4> */}
                         <h3>{`${party.start_date}`}</h3>
+                    </div>
+                    <div>
+                        <NavLink to="/" exact={true} activeClassName="active">
+                            <Button variant="contained" color="secondary"
+                            >Login</Button>
+                        </NavLink>
+                        <Button />
                     </div>
                 </div>
             </NavLink>
