a40812fb7aff67f6e505b26ef5f25f720bb23f1c
aleksandar <aca.djordjevic1113@gmail.com>
2020-12-22 15:41:39 -0800

    added date picker but still norrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrporiuryrtrjrehsrtryjukirrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr functiona

---
+++ react-app/src/components/DatePicker.js
@@ -0,0 +1,43 @@
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+const DatePicker = ()  =>{
+    // The first commit of Material-UI
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
+    return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+                <KeyboardDatePicker
+                    margin="normal"
+                    id="date-picker-dialog"
+                    label="Date picker dialog"
+                    format="MM/dd/yyyy"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                />
+            </Grid>
+        </MuiPickersUtilsProvider>
+    );
+}
+
+
+// const  DatePickerContainer = () => {
+
+// }
+
+
+export default DatePicker
--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import NavBarContainer from './NavBar';
 import HomePartiesContainer from './HomeParties';
+import DatePicker from './DatePicker'
 
 
 
@@ -11,6 +12,7 @@ const Home = ({ authenticate, setAuthenticated}) => {
     return (
     <div> 
         <NavBarContainer authenticate={authenticate} setAuthenticated={setAuthenticated}/>
+        <DatePicker />
         <HomePartiesContainer />
         {/* <Footer /> */}
     </div>
