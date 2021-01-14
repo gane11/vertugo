dfc29308514f76876d608a60304b399da71fb78d
aleksandar <aca.djordjevic1113@gmail.com>
2021-01-14 09:26:34 -0800

    added new calendar

--- react-app/src/components/DatePicker.js
+++ react-app/src/components/DatePicker.js
@@ -1,44 +1,77 @@
-import 'date-fns';
+// import 'date-fns';
 import React, {useState} from 'react';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-    MuiPickersUtilsProvider,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
+// import Grid from '@material-ui/core/Grid';
+// import DateFnsUtils from '@date-io/date-fns';
+// import {
+//     MuiPickersUtilsProvider,
+//     KeyboardDatePicker,
+// } from '@material-ui/pickers';
 
-const DatePicker = ()  =>{
+// const DatePicker = ()  =>{
   
-    const [selectedDate, setSelectedDate] = useState(new Date());
+//     const [selectedDate, setSelectedDate] = useState(new Date());
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-    };
-
-
-    return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-                <KeyboardDatePicker
-                    margin="normal"
-                    id="date-picker-dialog"
-                    // label=""
-                    format="MM/dd/yyyy"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                    }}
-                />
-            </Grid>
-        </MuiPickersUtilsProvider>
-    );
-}
+//     const handleDateChange = (date) => {
+//         setSelectedDate(date);
+//     };
 
 
-// const  DatePickerContainer = () => {
-
+//     return (
+//         <MuiPickersUtilsProvider utils={DateFnsUtils}>
+//             <Grid container justify="space-around">
+//                 <KeyboardDatePicker
+//                     margin="normal"
+//                     id="date-picker-dialog"
+//                     // label=""
+//                     format="MM/dd/yyyy"
+//                     value={selectedDate}
+//                     onChange={handleDateChange}
+//                     KeyboardButtonProps={{
+//                         'aria-label': 'change date',
+//                     }}
+//                 />
+//             </Grid>
+//         </MuiPickersUtilsProvider>
+//     );
 // }
 
 
-export default DatePicker
+// // const  DatePickerContainer = () => {
+
+// // }
+
+
+// export default DatePicker
+
+
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+export default function Example() {
+    const [selectedDay, setSelectedDay] = useState(new Date())
+
+    const handleDayClick = (date) => {
+        setSelectedDay(date)
+    }
+
+    console.log(selectedDay)
+
+    return (
+        <div>
+        <DayPicker
+            selectedDays={selectedDay}
+            onDayClick={handleDayClick}
+            initialMonth={new Date()}
+            fromMonth={new Date()}
+            disabledDays={[
+                {
+                    before: new Date(),
+                }
+            
+            ]}
+        />
+        <p>
+        </p>
+        </div>
+    );
+}
