614e0f02b453759232736e6ae8a7fcbe1a553875
aleksandar <aca.djordjevic1113@gmail.com>
2021-02-16 17:25:05 -0800

    styling splash 2

--- react-app/src/components/splash/Splash.js
+++ react-app/src/components/splash/Splash.js
@@ -6,7 +6,18 @@ const Splash = () => {
     return(
         <div className="splash__container">
             <div className="splash-header__container">
-                <div className="splash-header__logo"></div>
+                <div className="splash-header__logo">
+                </div>
+                <div className="splash-header__right">
+                    
+
+                    <div className="splash-header__home">
+                    Home
+                </div>
+                    <div className="splash-header__club-owner">
+                    Club Owner
+                </div>
+                </div>
 
             </div>
             <div className="splash-top__container" >
