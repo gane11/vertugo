e7febeb5f93a3d44db6120334e8ab73515814b73
aleksandar <aca.djordjevic1113@gmail.com>
2021-02-18 16:13:50 -0800

    added gif1

--- react-app/src/components/splash/Splash.js
+++ react-app/src/components/splash/Splash.js
@@ -3,6 +3,7 @@ import {useDispatch, useSelector} from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import video1 from '../images/vertugo1.gif'
 AOS.init();
 
 AOS.init({
@@ -93,7 +94,7 @@ const Splash = () => {
                         1. Search for a party 
                     </div>
                     <div data-aos="fade-left" data-aos-delay="900" className="splash-middle__picture">
-                        computer here
+                        <img src={video1} />
                     </div>
 
                 </div>
