f6c0a8e0b562f5f261d67421acc457621caeef14
aleksandar <aca.djordjevic1113@gmail.com>
2021-02-20 16:24:24 -0800

    working on data-aos

--- react-app/src/components/splash/Splash.js
+++ react-app/src/components/splash/Splash.js
@@ -4,33 +4,14 @@ import { NavLink } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import gif1 from '../images/vertugo1.gif'
-AOS.init();
-AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 400, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: true, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
 
 
 
 const Splash = () => {
+    useEffect (() => {
+        AOS.init({})
+    }, [])
     return(
         <div className="splash__container">
             <div className="splash-header__container">
@@ -65,35 +46,35 @@ const Splash = () => {
                 </div>
                 <div className="splash-cities__container">
                     <NavLink className="splash-city" to={`/search/Miami`} exact={true} activeClassName="active">
-                    <div data-aos="fade-right" >Miami</div>
+                        <div data-aos-easing="ease-out-back"data-aos="fade-right" >Miami</div>
 
                     </NavLink>
                     <NavLink className="splash-city" to={`/search/Austin`}  exact={true} activeClassName="active">
-                    <div data-aos="fade-right" data-aos-delay="200">Austin</div>
+                        <div data-aos="fade-right" data-aos-easing="ease-out-back" data-aos-delay="200">Austin</div>
 
                     </NavLink>
                     <NavLink className="splash-city" to={`/search/San%20Francisco`}  exact={true} activeClassName="active">
-                    <div data-aos="fade-right" data-aos-delay="400">San Francisco</div>
+                        <div data-aos="fade-right" data-aos-easing="ease-out-back"data-aos-delay="400">San Francisco</div>
 
                     </NavLink>
                     <NavLink className="splash-city" to={`/search/Los%20Angeles`} exact={true} activeClassName="active">
-                    <div data-aos="fade-right" data-aos-delay="600">Los Angeles</div>
+                        <div data-aos="fade-right" data-aos-easing="ease-out-back"data-aos-delay="600">Los Angeles</div>
 
                     </NavLink>
                     <NavLink className="splash-city" to={`/search/New%20York`}  exact={true} activeClassName="active">
 
-                    <div data-aos="fade-right" data-aos-delay="800">New York</div>
+                        <div data-aos="fade-right" data-aos-easing="ease-out-back" data-aos-delay="800">New York</div>
                     </NavLink>
                 </div>
                 
             </div>
             <div className="splash-middle__container">
                 <div className="splash-middle__first">
-                    <div data-aos="fade-right" data-aos-delay="900" 
-                        data-aos-easing="ease-in-sine" className="splash-middle__text">
+                    <div data-aos="fade-right" data-aos-easing="ease-out-back"data-aos-delay="900" 
+                      className="splash-middle__text">
                         1. Search for a party 
                     </div>
-                    <div data-aos="fade-left" data-aos-delay="900" className="splash-middle__picture">
+                    <div data-aos-easing="ease-out-back"data-aos="fade-left" data-aos-delay="900" className="splash-middle__picture">
                         <img src={gif1} />
                     </div>
 
@@ -103,7 +84,7 @@ const Splash = () => {
                     <div  className="splash-middle__picture2">
                         <img src={gif1} />
                     </div>
-                    <div data-aos="fade-right" data-aos-delay="600" className="splash-middle__text2">
+                    <div data-aos="fade-in" className="splash-middle__text2">
                         2. Buy a ticket
                     </div>
 
