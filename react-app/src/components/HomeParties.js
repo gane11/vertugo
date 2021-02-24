346194b8c12e64649c10162be56252e5d069e4fd
aleksandar <aca.djordjevic1113@gmail.com>
2021-02-23 23:36:14 -0800

    cleanup8

--- react-app/src/components/HomeParties.js
+++ react-app/src/components/HomeParties.js
@@ -55,7 +55,7 @@ const HomeParties = ({getAllParties, parties, clubs, getAllClubs, savedParties,g
                     <div className="party__section">
                         {parties.map((party) => {
                         if (ids.includes(party.club_id) && new Date(party.start_date).toISOString().split('T')[0] === new Date(selectedDate).toISOString().split('T')[0]) {
-                        // console.log(new Date(party.start_date).toISOString().split('T')[0], new Date(selectedDate).toISOString().split('T')[0])
+                        
                             count+=1
                             return(
                         < Card party={party} clubs={clubs}/>
