129061d846e2a8fb490d642ae33539ffbc7e94a3
aleksandar <aca.djordjevic1113@gmail.com>
2021-03-06 14:58:22 -0800

    reseeding

--- react-app/src/components/DatePicker.js
+++ react-app/src/components/DatePicker.js
@@ -38,7 +38,7 @@ import { getDate } from '../store/actions/dateAction';
                 new Date(Date.now() - 864e5),
                 {
                     before: today
-                   
+                   React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library[3] for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies.[4][5][6] React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing.[7][8] React Router[9] is an example of such a library.u
                 }
             
             ]}
